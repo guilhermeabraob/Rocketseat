3777856b83a33cdbb64e49326146e41101181a0f
guilhermeabraob <guilhermeabraob@hotmail.com>
2022-05-16 09:55:27 -0300

    Rocketseat

--- JS/script.js
+++ JS/script.js
@@ -1,113 +1,103 @@
-//# MANIPULANDO DADOS
+//#EXPRESSOES E OPERADORES
 
-/* 
-* Prototype (prototipos = aquilo que foi criado antes)
-    * O JS é prototype-based language
-    * prototype chain (cadeia de prototipos)
-    * __proto__
+/* - Expressions
+   - Operators
+        *Binary
+        *Unary
+        *Ternary
 */
 
-// A maioria dos dados em JS possuem prototipos, ou seja, mesmo que criamos um objeto vazio, esse objeto vazio, puxará propriedades e metodos de outro objeto. 
+// Expressions
+// - New: Conhecida como left-hand-side expression. Serve para criar um novo objeto. 
 
------------
+let age = new Number(34)
+console.log(age)
 
-//#Type Conversion and Type Coersion
+//Operadores Unários
+// - Typeof: indica qual tipo de dado
+// - Delete: deleta uma propriedade de dentro de um objeto. Ex:
 
-//#Type Conversion, nós podemos alterar o tipo do dado, usando uma função específica. 
+const person = {
+    name: 'Gui',
+    age: 34,
+}
 
-//#Type Coersion, é quando o próprio JS altera o tipo do dado de forma coercitiva. 
+delete person.age
+ console.log(person)
 
------------
+ //Operadores Aritiméticos
 
-//Manipulando String e Numeros
+ //multiplicação 
+ console.log(3*3)
+ //divisão
+ console.log(4/2)
+ //exponencial 
+ console.log(3**3)//resultado = 27
 
-//Transformando String em Número
+ //resto da divisão
+ let remainder = 11 % 3// remainder = resto
+//resultado = 2, pois depois de dividir 11 por 9, sobra 2. 
 
-let string = '123'
-console.log(Number(string))
+//incremento
+let increment = 0
+increment++ // incremento sempre adicionará 1 // resultado = 1, pois tinhamos 0, adicionamos 1
 
-//Transformando Número em String
-//ATENÇAO: TIPO NUMBER TEM A COR DIFERENTE DE STRING. O NUMBER É NORMALMENTE ROXO NO CONSOLE. 
+//decrement
+let decrement = 0
+decrement-- //sempre reduzirá 1. 
 
-let Number = 321
-console.log(String(number))
+//Grouping operator ( ), os parênteses dão preferência para os que estão dentro dele. 
 
--------
+let total = (2+3) * 5
 
-//Contar quantos caracteres tem uma palavra e quantos dígitos tem um número. 
+//Operadores de Comparação
 
-let word = 'paralelepipedo'
-console.log(word.length)
+// == igual a
+// != diferente de 
+// === estritamente igual a. Aqui, comparamos tanto o valor, como também, o tipo do dado. Ex:
+let one = 1 // a var one recebe o valor de 1. 
+console.log(one === '1') //resultado = False, pois one tem valor 1 (numerico) e o console ta comparando 1 numerico com 1 (string)
 
-//para saber quantos dégitos tem um número, devemos primeiramente transformá-lo em uma string, pois Número não funciona com o Length.
+// !== estritamente diferente de
 
-let number = 123
-console.log(String(number).length)
+// >= maior que
+let one = 1
+console.log(one >= 1) // resultado = true, pois o one não é maior, mas ele é igual a 1. 
 
-//Transformando número quebrado com 2 casas decimais e trocar PONTO por VÍRGULA. 
+----------
 
-let number = 121.31239124832
-console.log(number.toFixed(2).replace('.', ','))
+//Operadores de Atribuição (assignment)
 
----------
+//assignment
+x = 1
+//addition assignment
+x += 2
+console.log(x) //resultado = 3, pois esse metodo de adicionar na atribuição é a mesma coisa que escrever: x = x + 2
 
-//Transformar letras em tudo minusculas ou vice-versa. 
+//subtraction assignment 
+x -= 2 
+//multplication assignment
+x *= 2
+//division assignment
+x /= 2 
+//remainder assignment
+x %= 2
+//exponentiation
+x **= 2
 
-let word = 'programar é muito legal!'
-console.log(word.toUpperCase())
-confirm.log(word.toLowerCase())
+------
 
-//Verificar se o texto contém a palavra: Amor.
-
-let phrase = "Eu quero viver o Amor"
-console.log(phrase.includes("Amor"))
-
----------
-
-//Separar um texto que contem espaços em um novo ARRAY onde cada texto é uma posição do array. 
-let phrase = 'Eu quero viver um novo amor'
-let myArray = phrase.split(" ") //aqui dentro do parenteses tem um espaço entre as aspas, significando que a frase sera separada por espaços. 
-
-//Substituir os espaços do array por underscore ( _ )
-let phrasesWithUnderscore = myArray.join('_')
-
----------
-
-//Criando Array com construtor
-let myArray = new Array('a', 'b', 'c')
-
-//Contando os Elementos de um ARRAY. Obs: No ARRAY, podemos inserir qualquer tipo de dado. No ex abaixo, temos uma string, um objeto e uma função. 
-
-console.log([
-    'a', {type: 'array'}, 
-    function () {return 'alo'}
-].length[2]) //dentro dessas ultimas conchetes, escolhemos o 2 para ver quem esta na posição 2 dentro do aray, poderia ser qualquer posição. 
+// Operadores Lógicos
+// And &&
+// Or ||
+// Not ! , aqui se vc negar algum dado, voce mudara o valor desse dado, se era true, passa a ser false, e vice-versa.  
 
 --------
 
-// Transformando uma cadeia de caracteres em elementos de um Array. 
+//Operador Condicional
 
-let word = "manipulação"
-console.log(Array.from(word))
+//Condition = condição
+// ? = então
+// : = se não
 
----------
-
-
-//Manipulando Array
-let techs = ['html', 'css', 'js']
-
-//Adicionar um item no fim
-techs.push('nodejs')
-//Adicionar no começo
-techs.unshift('sql')
-//remover do fim
-techs.pop('nodejs')
-//remover do começo
-techs.shift('sql')
-//pegar somente alguns elementos do array
-console.log(techs.slice(1,3)) // o (1) de dentro do parenteses indica qual pedaço voce quer retirar do array, e o (3), indica até qual elemento você quer que mantenha. Atenção! Aqui é o elemento, e não a posição. 
-
-//remover um ou mais itens em qualquer posição do aray
-techs.splice(1,2)
-//encontrar a posição de um elemento dentro do array
-let index = techs.indexOf('css')
+//Estrutura. condition ? value 1 : value 2. Obs: Leitura, se a contição for respeitada, teremos um valor verdadeiro, caso contrário, teremos falso. 
